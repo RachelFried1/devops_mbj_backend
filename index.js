48a0e50de0d1d7627291a075f1907b87a17e71cd
RachelFried1 <rachelfried0828@gmail.com>
2024-12-11 16:25:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+
 const express = require('express');
 const cors = require('cors'); 
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 // Enable CORS for all routes
 app.use(cors());
@@ -11,6 +11,10 @@ app.get('/api', (req, res) => {
     res.send({ message: 'Hello from backend!' });
 });
 
+app.get('/', (req, res) => { // For health check
+    res.status(200).json({ status: 'UP' });
+  });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
